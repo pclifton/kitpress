33a03df21f42440ae1d977260c5c26e04c8d4d03
Philip Clifton <p.clifton@squawk1200.net>
2018-08-08 07:32:49 -0500

    Ensure swapping is enabled when installing composer

--- ansible/roles/web/files/install-composer.sh
+++ ansible/roles/web/files/install-composer.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Ensure swap is set up
+if [ ! -f /var/swap.1 ]; then
+	/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+	chmod 600 /var/swap.1
+	/sbin/mkswap /var/swap.1
+	/sbin/swapon /var/swap.1
+fi
+
 EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE="$(php -r "echo hash_file('SHA384', 'composer-setup.php');")"
