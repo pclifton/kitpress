709e55a98d493fbb11c32dc8cd741230ebb0bf06
Philip Clifton <p.clifton@squawk1200.net>
2019-02-03 00:15:58 -0600

    Flesh out dev playbook
    Ansible will now set up dev servers with:
    * Bootstrapped blog data and images
    * Kitpress theme (installed and enabled)

--- ansible/roles/dev/files/ensure-wp-install.sh
+++ ansible/roles/dev/files/ensure-wp-install.sh
@@ -11,7 +11,23 @@ fi
 # OK, create the database
 wp db create > /dev/null
 
+# do the multisite install
 wp core multisite-install --url="http://kitpress-dev" --title="KitPress" --admin_user="pclifton" --admin_password="devpass" --admin_email="bogus@kitpress.net" --subdomains --skip-config > /dev/null
+
+# copy over the bootstrap upload stuff
+rm -rf /var/www/vhosts/default/wp-content/uploads
+cp -R /home/ec2-user/wp_bootstrap/uploads /var/www/vhosts/default/wp-content
+
+# import bootstrap post data
+wp post delete 1 --force
+wp comment delete 1 --force
+wp plugin install wordpress-importer --activate
+wp import /home/ec2-user/wp_bootstrap/wp_dump.xml --authors=skip --quiet --skip=attachment
+
+# symlink and activate WP plugins and theme
+ln -s /media/sf_kitpress-modules/kitpress-theme/src /var/www/vhosts/default/wp-content/themes/kitpress
+wp theme activate kitpress
+wp theme enable kitpress --network
 
 # Spit "changed" so ansible knows we did something
 echo "changed"
